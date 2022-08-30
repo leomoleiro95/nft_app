f23583b581e84ee3688df0067422045ed8dffe65
Leonardo Moleiro <lmoleiro@google.com>
2022-08-30 18:53:58 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Features from './Features'
+import Download from './Download'
+
+import SectionWrapper from './SectionWrapper.jsx'
+
+export {
+    Features,
+    Download,
+    SectionWrapper
+}
