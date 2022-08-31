c40113e6770826a6a78d1052fd4afd5d419b9a90
Leonardo Moleiro <lmoleiro@google.com>
2022-08-30 20:57:56 -0500

    Edited message

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ const App = () => {
   return (
     <>
       <SectionWrapper
-        title="You own store of Nifty NFTs. Start Selling & Growing"
-        description="Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        title="Your own store for buying, selling & trading NFTs"
+        description="Buy, store, collect NFTS, exchange & earn crypto. Join 10+ million people using Lmoleiro NFT Marketplace."
         showBtn
         mockupImg={assets.homeHero}
         banner="banner"
       />
       <SectionWrapper
         title="Smart User Interface Marketplace"
-        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        description="Experience a smooth UI of Lmoleiro NFT Marketplace. Smooth constant colors of a fluent UI design."
         mockupImg={assets.homeCards}
         reverse
       />
       <Features />
       <SectionWrapper
         title="Deployment"
-        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        description="Lmoleiro NFT is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
         mockupImg={assets.feature}
         reverse
       />
@@ -36,7 +36,7 @@ const App = () => {
 
       <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
         <p className={`${styles.pText} ${styles.whiteText}`}>
-          Made with love by <span className="bold">JavaScript Mastery</span>
+          Made with hard work, sweat (some tears) and love: by <span className="bold">Leonardo Moleiro</span>
         </p>
       </div>
     </>
